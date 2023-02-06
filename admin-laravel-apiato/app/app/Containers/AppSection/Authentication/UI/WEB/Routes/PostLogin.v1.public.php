f6df12dcc1e70afb9a0324c5ecf6a55042e29b93
Fedor <fedorvladimirov7@gmail.com>
2023-02-06 21:42:00 +0300

    new: emails

--- admin-laravel-apiato/app/app/Containers/AppSection/Authentication/UI/WEB/Routes/PostLogin.v1.public.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Containers\AppSection\Authentication\UI\WEB\Controllers\LoginController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('login', [LoginController::class, 'login'])
-    ->name('login_post_form');
-
---
+++ backend/admin/app/Models/RoleUser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+/**
+ * App\Models\UserRole
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|RoleUser newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|RoleUser newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|RoleUser query()
+ * @mixin \Eloquent
+ * @property int $id
+ * @property int $user_id
+ * @property int $role_id
+ * @method static \Illuminate\Database\Eloquent\Builder|RoleUser whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|RoleUser whereRoleId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|RoleUser whereUserId($value)
+ */
+class RoleUser extends Pivot
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public $timestamps = false;
+}
