d4e96b594e82e24de78291861eaf5a9d7113f0ae
Fedor <doworkupwell@gmail.com>
2023-04-04 11:57:44 +0300

    fix: providers emails

--- backend/emails/app/Providers/AuthServiceProvider.php
+++ backend/emails/app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 
 // use Illuminate\Support\Facades\Gate;
+namespace App\Providers;
+
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
--- backend/emails/app/Providers/BroadcastServiceProvider.php
+++ backend/emails/app/Providers/BroadcastServiceProvider.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 
+namespace App\Providers;
+
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\ServiceProvider;
 
