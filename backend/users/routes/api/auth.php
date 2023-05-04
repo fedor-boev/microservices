81cbfc580bc15d6a7eafd2cd76571180da23f602
Fedor <doworkupwell@gmail.com>
2023-05-04 10:17:17 +0300

    refactor: user auth routes

--- backend/users/routes/api/auth.php
+++ backend/users/routes/api/auth.php
@@ -2,17 +2,24 @@
 
 declare(strict_types=1);
 
-Route::post('login', [\App\Http\Controllers\Auth\LoginController::class, 'login'])->name('login');
-Route::post('register', [\App\Http\Controllers\Auth\RegisterController::class, 'register']);
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\User\UpdatePasswordController;
+use App\Http\Controllers\Auth\User\UpdateUserInfoController;
+use App\Http\Controllers\Auth\User\UserController;
+
+Route::post('login', [LoginController::class, 'login'])->name('login');
+Route::post('register', [RegisterController::class, 'register']);
 
 Route::group([
     'middleware' => 'auth:api',
 ], static function () {
-    Route::get('user', [\App\Http\Controllers\Auth\User\UserController::class, 'user']);
-    Route::patch('users/info', [\App\Http\Controllers\Auth\User\UpdateUserInfoController::class, 'updateInfo']);
-    Route::patch('users/password', [\App\Http\Controllers\Auth\User\UpdatePasswordController::class, 'updatePassword']);
+    Route::get('user', [UserController::class, 'user']);
+    Route::patch('user/info', [UpdateUserInfoController::class, 'updateInfo']);
+    Route::patch('user/password', [UpdatePasswordController::class, 'updatePassword']);
 
-    Route::get('admin', [\App\Http\Controllers\Auth\AuthController::class, 'authenticated'])->middleware('scope:admin');
-    Route::get('influencer', [\App\Http\Controllers\Auth\AuthController::class, 'authenticated'])->middleware('scope:influencer');
+    Route::get('admin', [AuthController::class, 'authenticated'])->middleware('scope:admin');
+    Route::get('influencer', [AuthController::class, 'authenticated'])->middleware('scope:influencer');
 });
 
