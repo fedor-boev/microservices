dae9e0c408b9feabfd05198da6421c09bee69956
Fedor <doworkupwell@gmail.com>
2023-05-25 23:48:18 +0300

    refactor: users

--- backend/users/app/Http/Controllers/User/UserController.php
+++ backend/users/app/Http/Controllers/User/UserController.php
@@ -6,5 +6,28 @@ use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
+    public function index()
+    {
 
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
 }
